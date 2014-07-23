fa9e31d38d00647c5c277cf0b540fe43e4bd86ff
segeis <mail@sebastiangeis.de>
2014-07-24 00:10:16 +0200

    Create Item added. Almost correct

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.order('purchased DESC, created_at').paginate(page: params[:page])
+    @items = Item.order('purchased, created_at DESC').paginate(page: params[:page])
   end
 
   # GET /items/1
